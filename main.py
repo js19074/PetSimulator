1cba896589b625dd856d8e0f9bddcf5f066cbf73
Jasnoor Singh <78879596+js19074@users.noreply.github.com>
2021-12-01 04:57:25 +0000

    I added comments about the purpose of each function

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-
+# Welcome Message
+print("Welcome to my Virus Simulator")
 
 # Data Creation
 def create_pet(names, weight):
-  '''Purpose: Prompts user input of name and weight to be stored Passed: names and weight list
-  Returns: Nothing'''
+    # '''Purpose: Prompts user input of name and weight to be stored 
+    #Passed: names and weight list
+    # Returns: Nothing'''
+    
     print("Enter the name of your first pet, or type XXX to quit")
     name_input = input()
 
@@ -18,9 +21,9 @@ def create_pet(names, weight):
 
 # Modifying/Adjusting Data
 def pet_action(names, weight, number_of_pets):
-  '''Purpose: Prompts user for decision of feed or exercise
-  Passed: names, weight and number of pets
-  Returns: Nothing'''
+    # '''Purpose: Prompts user for decision of feed or exercise
+    # Passed: names, weight and number of pets
+    # Returns: Nothing'''
     number_of_pets = len(names)
     for i in range(number_of_pets):
         print("F = feed | E = exercise | Q = quit")
@@ -32,22 +35,22 @@ def pet_action(names, weight, number_of_pets):
                 weight[i] += int(amount)
 
             elif action_input == "E":
-              print("Enter the amount you would like to exercise")
-              amount = input()
-              weight[i] -= int(amount)
+                print("Enter the amount you would like to exercise")
+                amount = input()
+                weight[i] -= int(amount)
 
             print("F = feed | E = exercise | Q = quit")
             action_input = input()
 
 # Output Data
 def pet_output(names, weight, number_of_pets):
-  '''Purpose: Displays stats of pet
-  Passed: names, weight and number of pets
-  Returns: Nothing'''
+    # '''Purpose: Displays stats of pet
+    # Passed: names, weight and number of pets
+    # Returns: Nothing'''
     for i in range(number_of_pets):
         print(names[i])
         if weight[i] > 200:
-          print("weights", weight[i], "and is unhappy with their weight")
+            print("weights", weight[i], "and is unhappy with their weight")
 
         elif weight[i] < 0:
             print("is starving and you should think about your actions")
@@ -57,9 +60,9 @@ def pet_output(names, weight, number_of_pets):
       
 # Data Storage
 def main():
-    '''Purpose: Runs through correct sequence 
-    Passed: Nothing
-    Returns: Nothing'''
+    # '''Purpose: Runs through correct sequence 
+    # Passed: Nothing
+    # Returns: Nothing'''
     names = []
     weight = []
     create_pet(names, weight)
